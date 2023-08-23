faa35a4484e835186bb185374909ca8d9cfe3fd0
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-23 22:20:45 +0200

    feat(adjustments/tavern): add stairs and chimney

--- src/adjustments/tavern.ts
+++ src/adjustments/tavern.ts
@@ -1,14 +1,18 @@
 import { ArxMap } from 'arx-level-generator'
+import { addChimney } from '@/adjustments/tavern/addChimney.js'
+import { addRoof } from '@/adjustments/tavern/addRoof.js'
 import { addUpstairsFloor } from '@/adjustments/tavern/addUpstairsFloor.js'
 import { removeBlockedStairs } from '@/adjustments/tavern/removeBlockedStairs.js'
 import { removeCeiling } from '@/adjustments/tavern/removeCeiling.js'
 import { removeUpstairsFiller } from '@/adjustments/tavern/removeUpstairsFiller.js'
-import { addRoof } from './tavern/addRoof.js'
+import { addStairs } from './tavern/addStairs.js'
 
 export const adjustTavern = (map: ArxMap) => {
   removeCeiling(map)
   removeBlockedStairs(map)
   removeUpstairsFiller(map)
   addUpstairsFloor(map)
+  addChimney(map)
   addRoof(map)
+  addStairs(map)
 }
