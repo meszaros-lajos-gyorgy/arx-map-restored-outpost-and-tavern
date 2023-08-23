e21572206d3f2f39ac5cccf54286fb3a10fb3de7
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-23 22:16:31 +0200

    feat(textures): add all textures into a single file

---
+++ src/textures.ts
@@ -0,0 +1,18 @@
+import { Texture } from 'arx-level-generator'
+
+export const stoneHumanWall2a = new Texture({ filename: '[stone]_human_wall2a.jpg' })
+export const stoneHumanWall2b = new Texture({ filename: '[stone]_human_wall2b.jpg' })
+
+export const stoneHumanWall5a = new Texture({ filename: '[stone]_human_wall5a.jpg' })
+export const stoneHumanWall5b = new Texture({ filename: '[stone]_human_wall5b.jpg' })
+
+export const woodBlacksmithCeiling1 = new Texture({ filename: '[wood]_blacksmith_ceiling1' })
+export const woodBlacksmithCeiling2 = new Texture({ filename: '[wood]_blacksmith_ceiling2' })
+
+export const woodDoor = new Texture({ filename: '(wood) door.bmp' })
+
+export const woodHumanDirtPlanks = new Texture({ filename: '[wood]_human_dirt_planks.jpg' })
+
+export const woodFloor = new Texture({ filename: '[wood]_floor.jpg' })
+
+export const woodWood2 = new Texture({ filename: '[wood]wood2.jpg' })
