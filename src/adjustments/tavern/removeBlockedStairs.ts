17bf9bb334a08eb3d1f760f4ef577bc5b5e33043
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-23 22:19:33 +0200

    feat(adjustments/tavern/removeBlockedStairs): remove old crooked ramp from above Tizzy

--- src/adjustments/tavern/removeBlockedStairs.ts
+++ src/adjustments/tavern/removeBlockedStairs.ts
@@ -3,7 +3,9 @@ import { Box3 } from 'three'
 import { removePolygonsWithinBox } from '@/tools/removePolygonsWithinBox.js'
 
 export const removeBlockedStairs = (map: ArxMap) => {
-  const box = new Box3(new Vector3(9700, 850, 12920), new Vector3(9900, 1110, 13400))
+  const box1 = new Box3(new Vector3(9700, 850, 12920), new Vector3(9900, 1110, 13400))
+  const box2 = new Box3(new Vector3(9800, 800, 12980), new Vector3(10200, 1106, 13355))
 
-  removePolygonsWithinBox(box, map)
+  removePolygonsWithinBox(box1, map)
+  removePolygonsWithinBox(box2, map)
 }
