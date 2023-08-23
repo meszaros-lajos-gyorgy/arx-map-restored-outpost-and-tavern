66daa4d2f5335f8135b5b51e1229fe3e5a29076e
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-23 22:20:12 +0200

    refactor(adjustments/tavern/removeCeiling): use textures from textures file instead of hosting them directly

--- src/adjustments/tavern/removeCeiling.ts
+++ src/adjustments/tavern/removeCeiling.ts
@@ -1,9 +1,7 @@
 import { ArxMap, Texture, Vector3 } from 'arx-level-generator'
 import { Box3 } from 'three'
+import { woodBlacksmithCeiling1, woodBlacksmithCeiling2 } from '@/textures.js'
 import { removePolygonsByTextureWithinBox } from '@/tools/removePolygonsByTextureWithinBox.js'
-
-const woodBlacksmithCeiling1 = new Texture({ filename: '[wood]_blacksmith_ceiling1' })
-const woodBlacksmithCeiling2 = new Texture({ filename: '[wood]_blacksmith_ceiling2' })
 
 export const removeCeiling = (map: ArxMap) => {
   const box = new Box3(new Vector3(8100, 800, 12000), new Vector3(8100 + 2500, 830, 13600))
