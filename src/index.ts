1ad4d06339227ed0c6e101e518baaa72f51f456b
Lajos Meszaros <m_lajos@hotmail.com>
2023-08-27 17:47:54 +0200

    feat(Settings): let arx-level-generator parse the .env for us

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,7 @@ import { adjustAll } from '@/adjustments/all.js'
 import { adjustOutpost } from '@/adjustments/outpost.js'
 import { adjustTavern } from '@/adjustments/tavern.js'
 
-const settings = new Settings({
-  levelIdx: parseInt(process.env.levelIdx ?? '12'),
-  outputDir: process.env.outputDir,
-  seed: process.env.seed,
-  variant: process.env.variant === 'premium' ? 'premium' : 'normal',
-  calculateLighting: process.env.calculateLighting === 'false' ? false : true,
-  mode: process.env.mode === 'development' ? 'development' : 'production',
-  originalLevelFiles: process.env.originalLevelFiles,
-})
+const settings = new Settings()
 
 const map = await ArxMap.fromOriginalLevel(12, settings)
 
